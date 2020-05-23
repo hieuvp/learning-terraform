50cfbc1b91f58a02d219113a9b3dd99febccfff3
Harrison Van <harrison.van@shopback.com>
2020-05-23 10:34:37 +0700

    cleanup

--- terraform-and-packer/using-packer-for-building-custom-amis/scripts/cleanup.sh
+++ terraform-and-packer/using-packer-for-building-custom-amis/scripts/cleanup.sh
@@ -1,16 +1,33 @@
 #!/usr/bin/env bash
 
+set -eou pipefail
+
 export AWS_REGION="us-east-1"
 
 readonly AMI_NAME="shopback-linux-*"
+readonly IMAGES=$(
+  aws ec2 describe-images --output json \
+    --filters "Name=name,Values=${AMI_NAME}"
+)
 
-set -eoux pipefail
+main() {
+  local -r index=$1
+  local -r name=$(echo "$IMAGES" | jq --raw-output ".Images[${index}].Name")
+  local -r image_id=$(echo "$IMAGES" | jq --raw-output ".Images[${index}].ImageId")
+  local -r snapshot_id=$(echo "$IMAGES" | jq --raw-output ".Images[${index}].BlockDeviceMappings[0].Ebs.SnapshotId")
 
-aws ec2 describe-images \
-  --filters "Name=name,Values=${AMI_NAME}" \
-  --output json
+  printf "\n"
+  echo "Found A Matching AMI       : ${name}"
 
-# If packer doesn't have cleanup functions
-# This script will do cleanup resources
-# - AMIs
-# - EC2 instances
+  echo "- Deregistering This Image : ${image_id}"
+  # aws ec2 deregister-image --image-id ami-0123456789
+
+  echo "- Deleting This Snapshot   : ${snapshot_id}"
+  # aws ec2 delete-snapshot --snapshot-id snap-9876543210
+}
+
+for index in $(echo "$IMAGES" | jq '.Images | keys | .[]'); do
+  main "$index"
+done
+
+printf "\n"
